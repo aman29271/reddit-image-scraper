a46136d8b725d4f58a412dd191e019ca757f1848
Aman <aman29271@gmail.com>
2019-10-18 15:24:38 +0530

    removing duplicates

--- getImage.js
+++ getImage.js
@@ -1,19 +1,26 @@
 const path = require("path");
 const fs = require("fs");
 const request = require("request");
-const chalk = require('chalk')
+const chalk = require("chalk");
 // console.log(process.argv[2],'-->',process.argv[3]);
 
-function getIndivisualImage(url,dir) {
+function getIndivisualImage(url, dir) {
   const fileName = path.basename(url);
-  const regex = /https?:\/\/i\.redd\.it\/.*\.(jpg|png|gif)/
+  const regex = /https?:\/\/i\.redd\.it\/.*\.(jpg|png|gif)/;
+
+  const relativeDir = path.join(dir, fileName);
+
 
   if (url.match(regex)) {
-    request(url)
-      .pipe(fs.createWriteStream(`${dir}/${fileName}`))
-      .on("close", function() {
-        console.log(url, chalk.green(' downloaded successfully.'));
-      });
+    if (fs.existsSync(relativeDir)) {
+      console.log(fileName,chalk.yellow(" Already exist. Skipping..."));
+    } else {
+      request(url)
+        .pipe(fs.createWriteStream(`${dir}/${fileName}`))
+        .on("close", function() {
+          console.log(url, chalk.green(" downloaded successfully."));
+        });
+    }
   }
 }
 // getIndivisualImage(process.argv[2])
